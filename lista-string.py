2cefa07006a185e77db4f90cff7c9259c4aef622
David Arteaga <david17art@gmail.com>
2017-03-24 09:20:35 -0400

    Clase:4 String2 lista de string

---
+++ lista-string.py
@@ -0,0 +1,7 @@
+my_string = "Curso de Codigo Facilito"
+print(my_string)
+print(my_string[0]) #Con esto obtenemos el primer caracter de la cadena.
+print(my_string[-1]) #anticipando un negativo es Para recorrer la cadena de derecha a izquierda
+print(my_string[0:16]) #Para recorrer un rando de valores de una cadena
+print(my_string[0:10:2]) #Tambien se puede de forma de saltos progresivos
+print(my_string[::-1]) #Hacemos de reverse de esta forma
