d55fce79055e0960ac0c175872db2ae73efda8bd
David Arteaga <david17art@gmail.com>
2017-03-24 08:37:08 -0400

    Clase 1: Variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+#Las Variables no se comienza en mayuscula, todo debe ser en minusculas
+#Si queremos hacerla mas expresivas, usar guion bajo
+curso_codigo_facilito = "El Curso es Python 3"
+#Python tiene tipado dinamico, osea puedo declarar variables con el mismo nombre pero con diferentes valores
+curso_codigo_facilito = 99
+curso_codigo_facilito = True
+#El valor que arrojara es el valor de la ultima vez declarada
+print( curso_codigo_facilito )
