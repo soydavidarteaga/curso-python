6cee916ecd17c3a0644f5f6a0fec55fef9ed396b
David Arteaga <david17art@gmail.com>
2017-03-24 08:47:55 -0400

    Clase2: Tipos de Numeros

---
+++ numbers.py
@@ -0,0 +1,15 @@
+#En python existen distintos tipos de numeros. Enteros, Flotantes, Complejos (Complex), Boleanos
+number_one = 10
+number_two = 4
+result = number_one + number_two
+print( "Suma ", result ) 
+result = number_one - number_two
+print( "Resta ", result ) 
+result = number_one * number_two
+print( "Multiplicacion ", result ) 
+result = number_one / number_two
+print( "Division ", result ) #Regresa un numero flotante
+result = number_one // number_two
+print( "Division Numero entero ", result ) #Regresa una division con numero entero
+result = number_one ** number_two
+print( "Exponencial ", result ) #Regresa un numero a su exponencial 
