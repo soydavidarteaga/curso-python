dbdc4ee3f07fd203f1f1b808ff58577983e49d24
David Arteaga <david17art@gmail.com>
2017-03-24 09:57:15 -0400

    Clase:7 Tuplas

---
+++ tuplas.py
@@ -0,0 +1,5 @@
+''' Las tuplas son un tipo de dato en python, son casi iguales que los arreglos, pero inmutables '''
+my_tuple = (1,"string", True)
+print(my_tuple)
+print(my_tuple[1]) #Tambien se puede acceder a el indice
+#Cuando se utiliza? Siempre que queremos que los datos no sean cambiados, ya que no sera modificados
