f1c605c44c4191c9614c3fbd20315eafbb0c3819
David Arteaga <david17art@gmail.com>
2017-03-27 09:25:11 -0400

    Clase:15 Funciones 3

--- funciones.py
+++ funciones.py
@@ -7,10 +7,10 @@ def factorial_numero(numero):
 		numero -=1
 	return factorial
 
-'''
+
 print factorial_numero(4)
 print factorial_numero(5)
-print factorial_numero(6) '''
+print factorial_numero(6) 
 
 #Fin Clase 1
 
@@ -41,4 +41,35 @@ print mi_variable(6,8)
 def mostrar_resultado( funcion ):
 	print(funcion(6,8))
 mostrar_resultado(mi_variable)
+
+#Fin Clase 2
+
+#Clase 3
+
+frase = 'anita lava la tina' #Variables Globales
+def palindromo():
+	valor = frase.replace(' ','') #Variables Locales
+	return valor == valor[::-1]
+
+resultado = palindromo()
+print(frase)
+print resultado
+
+
+variable_global = 'Esto es un variable Global'
+
+def valor_global():
+	global variable_global
+	variable_global = 'Cambio de valor'
+
+print(variable_global)
+valor_global()
+print(variable_global)
+
+def crear_global():
+	global nueva_variable
+	nueva_variable = 'Esto es una variable global'
+
+crear_global()
+print(nueva_variable)
 
