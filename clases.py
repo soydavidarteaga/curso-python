84d0d89a64625e2042e955eb92f774fbb32c9a8d
David Arteaga <david17art@gmail.com>
2017-03-29 10:28:27 -0400

    Clase:28 Atributos y Metodos

--- clases.py
+++ clases.py
@@ -1,10 +1,25 @@
 #Las clases deben comenzar en mayuscula
 #Si son mas de dos plabras la conversion debe ser asi "LapizAmarillo"
 class Lapiz:
+	#Atributos
 	color = 'Amarillo'
-	contiene = False
+	contiene_borrador = False
 	usa_graffito = True
+	#Metodos
+	def dibujar(self):
+		print("El lapiz esta dibujando")
+	def borrar(self):
+		if self.es_valido_para_borrar():
+			print("El lapiz esta borrando")
+		else:
+			print("No es posible borrar")
+		
+	def es_valido_para_borrar(self):
+		return self.contiene_borrador 
+
 
 #Esto es un objeto
 lapiz_generico = Lapiz()
+lapiz_generico.dibujar()
+lapiz_generico.borrar()
 print (lapiz_generico.color)
