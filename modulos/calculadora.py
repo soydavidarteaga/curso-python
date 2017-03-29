4a953d1938e2d0e623baf1cfae88001db023d1f2
David Arteaga <david17art@gmail.com>
2017-03-29 09:15:20 -0400

    Clase:23 Modulos: Estructura

--- modulos/calculadora.py
+++ modulos/calculadora.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python3
+"""
+Este modulo sirve para hacer las operaciones fundamentales
+"""
+__name__ = "calculadora"
+__author__="David Arteaga"
+__copyright__ = "Copyright 2017, Codigo Facilito"
+__credits__="Codigo Facilito"
+__licence__ = "GPL"
+__version__="1.0.1"
+__maintainer__="David Arteaga"
+__email__="david17art@gmail.com"
+__status__="Developer"
+
+
 def suma(n1,n2):
 	"""Regresa el resultado de la suma de dos numeros"""
 	return n1+n2
