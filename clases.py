4b5b8e09bed2a3939acf106dad2fedc87059a4e1
David Arteaga <david17art@gmail.com>
2017-03-29 10:17:49 -0400

    Clase:27 Clases y Objetos

---
+++ clases.py
@@ -0,0 +1,10 @@
+#Las clases deben comenzar en mayuscula
+#Si son mas de dos plabras la conversion debe ser asi "LapizAmarillo"
+class Lapiz:
+	color = 'Amarillo'
+	contiene = False
+	usa_graffito = True
+
+#Esto es un objeto
+lapiz_generico = Lapiz()
+print (lapiz_generico.color)
